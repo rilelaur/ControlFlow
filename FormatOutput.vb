9f12789aa15355148696addaad165d4f0b45d8f3
Laura <laurakriley01@gmail.com>
2021-01-26 08:48:21 -0700

    Format Output

---
+++ FormatOutput.vb
@@ -0,0 +1,23 @@
+﻿Option Explicit On
+Option Strict On
+
+Module FormatOutput
+    Sub main()
+
+        Dim temp As String
+        For j = 0 To 10
+            For i = 0 To 10
+                temp = i.ToString("D5")
+                Len(temp)
+                'Console.WriteLine($"The string: {temp.PadLeft(7)} is {Len(temp)} characters long")
+                If i = 5 Then
+                    temp = ""
+                End If
+                Console.Write(temp.PadRight(8))
+            Next
+            Console.WriteLine()
+        Next
+
+        Console.Read()
+    End Sub
+End Module
